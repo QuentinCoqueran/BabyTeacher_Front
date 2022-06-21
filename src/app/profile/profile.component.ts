3cc4696cfcfba9f748e128d052b5ef793d01bab7
David <zdavid1@myges.fr>
2022-06-21 16:05:02 +0200

    profile / skills

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -18,10 +18,16 @@ export class ProfileComponent implements OnInit {
   public sexeUser: string = "";
   public sexe = "1";
   public lengthTextAreaNumber: number = 0;
-  public modificationUserBool: boolean = false;
   public saveDisabled: boolean = false;
   public loginParam: string = "";
   public samePeople: boolean = false;
+  public displaySkillsListBool: boolean = false;
+  public displayAgendaBool: boolean = false;
+  public displayInformationsBool: boolean = false;
+  public modificationUserBool: boolean = false;
+  public modificationSkillsBool: boolean = false;
+
+  public skills: [{ skills: string, categorie: string }] = [{skills: '', categorie: ''}];
   Object = Object;
   pictureProfile: string = "../assets/avatar.png";
 
@@ -29,8 +35,16 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initUserByLogin()
-    this.initUserByToken();
+    this.route.queryParams.subscribe(async params => {
+      this.loginParam = params['login'];
+      if (!this.loginParam) {
+        await this.router.navigate(['/login']);
+        return;
+      }
+      await this.initUserByLogin()
+      await this.initUserByToken();
+      this.initSkills();
+    });
   }
 
   async initUserByToken() {
@@ -39,7 +53,6 @@ export class ProfileComponent implements OnInit {
       for (let i = 0; i < Object.keys(userService).length; i++) {
         if (Object.keys(userService)[i] == 'login') {
           this.samePeople = this.loginParam == Object.values(userService)[i];
-          console.log(this.samePeople);
         }
       }
     } else {
@@ -49,67 +62,59 @@ export class ProfileComponent implements OnInit {
   }
 
   async initUserByLogin() {
-    this.route.queryParams.subscribe(async params => {
-      this.loginParam = params['login'];
-      if (!this.loginParam) {
-        await this.router.navigate(['/login']);
-        return;
-      }
-      let userService = await this.authService.getUserByLogin(this.loginParam);
-      if (userService) {
-        for (let i = 0; i < Object.keys(userService).length; i++) {
-          if (Object.keys(userService)[i] == "id") {
-            this.userId = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'login') {
-            this.user.login = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'lastname') {
-            this.user.lastname = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'name') {
-            this.user.name = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'email') {
-            this.user.email = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'photo') {
-            if (Object.values(userService)[i] === null) {
-              this.pictureProfile = "../assets/avatar.png";
-            } else {
-              this.pictureProfile = Object.values(userService)[i];
-            }
-          }
-          if (Object.keys(userService)[i] == 'description') {
-            this.user.description = Object.values(userService)[i];
-            if (Object.values(userService)[i])
-              this.lengthTextAreaNumber = Object.values(userService)[i].length;
-          }
-          if (Object.keys(userService)[i] == 'age') {
-            this.user.age = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'id_role') {
-            this.roleId = Object.values(userService)[i];
-          }
-          if (Object.keys(userService)[i] == 'sexe') {
-            this.sexe = Object.values(userService)[i].toString();
-            if (Object.values(userService)[i] == 1) {
-              this.sexeUser = 'Homme';
-            } else {
-              this.sexeUser = 'Femme';
-            }
+    let userService = await this.authService.getUserByLogin(this.loginParam);
+    if (userService) {
+      for (let i = 0; i < Object.keys(userService).length; i++) {
+        if (Object.keys(userService)[i] == "id") {
+          this.userId = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'login') {
+          this.user.login = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'lastname') {
+          this.user.lastname = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'name') {
+          this.user.name = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'email') {
+          this.user.email = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'photo') {
+          if (Object.values(userService)[i] === null) {
+            this.pictureProfile = "../assets/avatar.png";
+          } else {
+            this.pictureProfile = Object.values(userService)[i];
           }
         }
-        await this.initRole();
-      } else {
-        await this.router.navigate(['/login']);
-        return;
+        if (Object.keys(userService)[i] == 'description') {
+          this.user.description = Object.values(userService)[i];
+          if (Object.values(userService)[i])
+            this.lengthTextAreaNumber = Object.values(userService)[i].length;
+        }
+        if (Object.keys(userService)[i] == 'age') {
+          this.user.age = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'id_role') {
+          this.roleId = Object.values(userService)[i];
+        }
+        if (Object.keys(userService)[i] == 'sexe') {
+          this.sexe = Object.values(userService)[i].toString();
+          if (Object.values(userService)[i] == 1) {
+            this.sexeUser = 'Homme';
+          } else {
+            this.sexeUser = 'Femme';
+          }
+        }
       }
-    });
+      await this.initRole();
+    } else {
+      await this.router.navigate(['/login']);
+      return;
+    }
   }
 
   async initRole() {
-    console.log(this.userId)
     let role = await this.authService.getRoleByToken(this.userId);
     if (role) {
       this.user.role = Object.values(role)[0]['role'];
@@ -126,6 +131,10 @@ export class ProfileComponent implements OnInit {
 
   modificationUser() {
     this.modificationUserBool = !this.modificationUserBool;
+  }
+
+  modificationSkills() {
+    this.modificationSkillsBool = !this.modificationSkillsBool;
   }
 
   modifySexe(value: string) {
@@ -176,6 +185,50 @@ export class ProfileComponent implements OnInit {
             this.returnError = true;
           }
         });
+    }
+  }
+
+  private initSkills() {
+    this.skills = [{skills: '', categorie: ''}];
+    this.authService.getSkills(this.user.login).then(
+      (data: any) => {
+        if (data.response) {
+          for (let i = 0; i < data.response.length; i++) {
+            if (this.skills[0].skills == '') {
+              this.skills[0] = {skills: data.response[i]["name"], categorie: data.response[i]["test"]};
+            } else {
+              this.skills.push({skills: data.response[i]["name"], categorie: data.response[i]["test"]});
+            }
+          }
+        } else {
+          this.errorMessage = "Erreur lors de la récupération des compétences";
+          this.returnError = true;
+        }
+      }
+    );
+  }
+
+  displaySkillsList() {
+    this.displaySkillsListBool = !this.displaySkillsListBool;
+    this.displayAgendaBool = false;
+    this.displayInformationsBool = false;
+  }
+
+  displayAgenda() {
+    this.displayAgendaBool = !this.displayAgendaBool;
+    this.displaySkillsListBool = false;
+    this.displayInformationsBool = false;
+  }
+
+  displayInformations() {
+    this.displayInformationsBool = !this.displayInformationsBool;
+    this.displaySkillsListBool = false;
+    this.displayAgendaBool = false;
+  }
+
+  updateSkills() {
+    if (this.modificationSkillsBool) {
+
     }
   }
 }
