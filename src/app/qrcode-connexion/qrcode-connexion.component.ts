03748069759d4d51bb983b280625517a7943631b
David <zdavid1@myges.fr>
2022-07-13 12:30:07 +0200

    connect qrcode

--- src/app/qrcode-connexion/qrcode-connexion.component.ts
+++ src/app/qrcode-connexion/qrcode-connexion.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ConnexionService} from "../services/connexion.service";
-import {Params, Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {UserConnect} from "../models/UserConnect";
+import {UserConnectQRCode} from "../models/UserConnectQRCode";
+import {SubscribeService} from "../services/subscribe-service";
+import {AvailabilityService} from "../services/availability.service";
+import {NgbRatingConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-qrcode-connexion',
@@ -9,49 +13,48 @@ import {UserConnect} from "../models/UserConnect";
   styleUrls: ['./qrcode-connexion.component.scss']
 })
 export class QrcodeConnexionComponent implements OnInit {
+  private idBabysitter: number =-1;
+  private idContract: number=-1;
 
-  constructor(private connexionService: ConnexionService, private router: Router) {
+  constructor(private authService: ConnexionService, private updateUserService: SubscribeService, private route: ActivatedRoute, private subscribeService: SubscribeService, private router: Router, private availableService: AvailabilityService, private config: NgbRatingConfig) {
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(async params => {
+      this.idBabysitter = parseInt(params['idBabysitter']);
+      this.idContract = parseInt(params['idContract']);
+    });
   }
 
 
   public returnError: boolean = false;
   public clickConnect: boolean = false;
+  public errorMessage = "";
 
   checkConnected(pseudo: string, password: string): void {
     this.clickConnect = true;
     if (pseudo.trim() != "" && password.trim() != "") {
-      let userConnect: UserConnect = new UserConnect(pseudo, password);
-      this.connexionService.connectUser(userConnect).subscribe(
+      let userConnectQRCode: UserConnectQRCode = new UserConnectQRCode(pseudo, password,this.idBabysitter,this.idContract);
+      this.authService.connectUserQrCode(userConnectQRCode).subscribe(
         async res => {
           if (res.response) {
-            if (res.response["token"] != undefined) {
-              this.returnError = false;
-              localStorage.setItem('token', res.response["token"]);
-              if (res.response["role"] === "babysitter") {
-                if (res.response["firstConnection"]) {
-                  await this.router.navigate(['/first-connection-babysitter']);
-                  return;
-                }
-              }
-              const queryParams: Params = {login: res.response["login"]};
-              await this.router.navigate(
-                ['/profile'],
-                {
-                  queryParams: queryParams,
-                });
+            this.returnError = false;
+            if (res.response["role"] === "parent") {
+
             } else {
               this.returnError = true;
+              this.errorMessage = "Vous n'êtes pas un parent";
             }
+
           } else {
             this.returnError = true;
+            this.errorMessage = "Votre pseudo ou votre mot de passe est incorrect";
           }
           this.clickConnect = false;
         },
         error => {
           this.returnError = true;
+          this.errorMessage = "Votre pseudo ou votre mot de passe est incorrect";
           this.clickConnect = false;
         });
     } else {
