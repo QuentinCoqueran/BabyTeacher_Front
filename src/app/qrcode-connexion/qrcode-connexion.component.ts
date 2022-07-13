9c8989367f4c388dac8f9bcdc73e342f17e81b5d
David <zdavid1@myges.fr>
2022-07-13 15:15:17 +0200

    qr code test prod

--- src/app/qrcode-connexion/qrcode-connexion.component.ts
+++ src/app/qrcode-connexion/qrcode-connexion.component.ts
@@ -6,6 +6,7 @@ import {UserConnectQRCode} from "../models/UserConnectQRCode";
 import {SubscribeService} from "../services/subscribe-service";
 import {AvailabilityService} from "../services/availability.service";
 import {NgbRatingConfig} from "@ng-bootstrap/ng-bootstrap";
+import {ContractService} from "../services/contract.service";
 
 @Component({
   selector: 'app-qrcode-connexion',
@@ -13,10 +14,11 @@ import {NgbRatingConfig} from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ['./qrcode-connexion.component.scss']
 })
 export class QrcodeConnexionComponent implements OnInit {
-  private idBabysitter: number =-1;
-  private idContract: number=-1;
+  private idBabysitter: number = -1;
+  private idContract: number = -1;
 
-  constructor(private authService: ConnexionService, private updateUserService: SubscribeService, private route: ActivatedRoute, private subscribeService: SubscribeService, private router: Router, private availableService: AvailabilityService, private config: NgbRatingConfig) {
+  constructor(private authService: ConnexionService, private updateUserService: SubscribeService, private route: ActivatedRoute, private subscribeService: SubscribeService, private router: Router,
+              private availableService: AvailabilityService, private config: NgbRatingConfig, private contractService: ContractService) {
   }
 
   ngOnInit(): void {
@@ -29,23 +31,25 @@ export class QrcodeConnexionComponent implements OnInit {
 
   public returnError: boolean = false;
   public clickConnect: boolean = false;
+  public updateHoursBool: boolean = false;
   public errorMessage = "";
+  public contract = []
+  public contractBool: boolean = false;
 
   checkConnected(pseudo: string, password: string): void {
     this.clickConnect = true;
     if (pseudo.trim() != "" && password.trim() != "") {
-      let userConnectQRCode: UserConnectQRCode = new UserConnectQRCode(pseudo, password,this.idBabysitter,this.idContract);
+      let userConnectQRCode: UserConnectQRCode = new UserConnectQRCode(pseudo, password, this.idBabysitter, this.idContract);
       this.authService.connectUserQrCode(userConnectQRCode).subscribe(
         async res => {
           if (res.response) {
             this.returnError = false;
-            if (res.response["role"] === "parent") {
-
+            if (res.response === "parent") {
+              this.updateHoursBool = true;
             } else {
               this.returnError = true;
               this.errorMessage = "Vous n'êtes pas un parent";
             }
-
           } else {
             this.returnError = true;
             this.errorMessage = "Votre pseudo ou votre mot de passe est incorrect";
@@ -54,7 +58,7 @@ export class QrcodeConnexionComponent implements OnInit {
         },
         error => {
           this.returnError = true;
-          this.errorMessage = "Votre pseudo ou votre mot de passe est incorrect";
+          this.errorMessage = "Erreur de connexion";
           this.clickConnect = false;
         });
     } else {
@@ -72,5 +76,29 @@ export class QrcodeConnexionComponent implements OnInit {
 
   closeAlert() {
     this.returnError = false;
+  }
+
+  updateHours(hoursToDone: string) {
+    this.contract = [];
+    let hours: number = parseInt(hoursToDone);
+    if (hours > 0 && hours < 24) {
+      this.contractService.updateHours(this.idContract, hours).then(
+        res => {
+          if (res['response']) {
+            console.log(res['response']);
+            this.contract = res['response'][0];
+            this.contractBool = true;
+          } else {
+            this.returnError = true;
+            this.updateHoursBool = false;
+            this.errorMessage = "Erreur de mise à jour des heures";
+          }
+        }
+      );
+    } else {
+      this.updateHoursBool = false;
+      this.returnError = true;
+      this.errorMessage = "Veuillez entrer un nombre supérieur à 0";
+    }
   }
 }
