fca8c3d57ba9112b69d39bd0d42469850bd22cba
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-07-18 00:34:01 +0200

    fix date signalement, contrat on dropdown menu for admin and profile button on signalement

--- src/app/admin-signalement-profile/admin-signalement-profile.component.ts
+++ src/app/admin-signalement-profile/admin-signalement-profile.component.ts
@@ -16,6 +16,7 @@ export class AdminSignalementProfileComponent implements OnInit {
   public idProfile: number;
   public userProfile: UserSubscribe;
   public usersInfo: UserSubscribe[] = [];
+  public dateInfo: string[] = [];
   public isLoaded: boolean = false;
   public userLoaded: boolean = false;
   public loginLoaded: boolean = false;
@@ -55,6 +56,9 @@ export class AdminSignalementProfileComponent implements OnInit {
           await this.adminService.getUserById(signalement.idSignaler).subscribe({
             next: (data: UserSubscribe) => {
               this.usersInfo[signalement.idSignaler] = data["user"];
+              if (signalement.id) {
+                this.dateInfo[signalement.id] = signalement.dateTime.substring(0,4) + "-" + signalement.dateTime.substring(5,7) + "-" + signalement.dateTime.substring(8,10) + " " + signalement.dateTime.substring(11,13) + ":" + signalement.dateTime.substring(14,16) + ":00";
+              }
               cpt++;
               if(cpt == this.signalements.length){
                 this.loginLoaded = true;
@@ -87,7 +91,13 @@ export class AdminSignalementProfileComponent implements OnInit {
 
   async goToSpecificProfile(id: number){
     console.log(id);
-    await this.router.navigate(['/profile'], {queryParams: {login: id}});
+    this.adminService.getUserById(id).subscribe({
+      next: (data: UserSubscribe) => {
+        this.router.navigate(['/profile'], {queryParams: {login: data["user"].login}});
+      },error: (error: any) => {
+        console.log("Pas de profil: " + error);
+      }
+    });
   }
 
 }
